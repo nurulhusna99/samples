c37c57a1405537603e4b97dea63175b3c53f902d
Nurulhusna99 <husna8212@gmail.com>
2021-03-19 17:17:01 +0800

    initial commit

---
+++ samples/src/samples/Location.java
@@ -0,0 +1,14 @@
+package samples;
+
+public class Location {
+
+	public Location() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
