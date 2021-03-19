0e2dbe38c5194e4e126dcafbdd8ce5cdc7b1208a
Nurulhusna99 <husna8212@gmail.com>
2021-03-19 17:33:13 +0800

    Initial commit

--- samples/src/samples/Location.java
+++ samples/src/samples/Location.java
@@ -12,10 +12,15 @@ public class Location {
 	}
 	
 	private String name;
+	private int locationId;
 	private int description;
 	
 	public String getName() {
 		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
